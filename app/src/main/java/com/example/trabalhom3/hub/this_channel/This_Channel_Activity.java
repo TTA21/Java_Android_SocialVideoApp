095e4a4bbc52ac27bbae6ee8ecd73cf6b5fe2ccc
TTA21 <lucasmateusgoncalves20@gmail.com>
2020-11-25 18:46:38 -0300

    Added a "This user" activity

--- app/src/main/java/com/example/trabalhom3/hub/this_channel/This_Channel_Activity.java
+++ app/src/main/java/com/example/trabalhom3/hub/this_channel/This_Channel_Activity.java
@@ -1,11 +1,25 @@
 package com.example.trabalhom3.hub.this_channel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.trabalhom3.R;
+import com.example.trabalhom3.hub.file_sender.Video_Viewer_Activity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -23,6 +37,92 @@ public class This_Channel_Activity extends AppCompatActivity {
 
         category_names = extras.getStringArrayList( "cat_names" );
         this_username = extras.getString( "this_usernm" );
+
+        update_params();
+
+    }
+
+    private ArrayList<String> videonames = new ArrayList<String>();
+
+
+    private Long sum_views = 0L;
+    private Long sum_likes = 0L;
+    private Long sum_dislikes = 0L;
+
+    private void update_params(){
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        Query query = rootRef.child("Video_URL").child( this_username );
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot ds : dataSnapshot.getChildren()) { ///Get all videonames
+
+                    videonames.add( ds.getKey() );
+                    Log.d("New videoname", ds.getKey() );
+
+                    sum_views += Long.valueOf(ds.child( "ViewCount" ).getValue().toString());
+                    sum_likes += Long.valueOf(ds.child( "Like_Num" ).getValue().toString());
+                    sum_dislikes += Long.valueOf(ds.child( "Dislike_Num" ).getValue().toString());
+
+                }
+
+                update_overlay();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("ERROR", databaseError.getMessage());
+            }
+        };
+        query.addValueEventListener( valueEventListener );
+
+    }
+
+    private void update_overlay(){
+
+        TextView totalviews_obj = findViewById( R.id.total_views );
+        TextView totallikes_obj = findViewById( R.id.total_likes );
+        TextView total_dislikes_obj = findViewById( R.id.total_dislikes );
+
+        totalviews_obj.setText( String.valueOf( sum_views ) + " Views no total" );
+        totallikes_obj.setText( String.valueOf( sum_likes ) + " Likes no total" );
+        total_dislikes_obj.setText( String.valueOf( sum_dislikes ) + " Dislikes no total" );
+
+        if( !videonames.isEmpty() ){
+
+            ListView list_videos = findViewById( R.id.this_user_videolist );
+            ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, videonames);
+            list_videos.setAdapter(arrayAdapter);
+            list_videos.setOnItemClickListener( listClick );
+
+        }
+
+        sum_views = 0L;
+        sum_likes = 0L;
+        sum_dislikes = 0L;
+
+        videonames = new ArrayList<String>();
+
+    }
+
+    private AdapterView.OnItemClickListener listClick = new AdapterView.OnItemClickListener(){
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            String itemAtPosition = String.valueOf( parent.getItemAtPosition(position) );
+
+            ////TODO : Make a way to change video names and categoies
+
+        }
+    };
+
+    public void exit_func(View view) {
+
+        finish();
 
     }
 }
